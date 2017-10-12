4d60a88c86f2d179cb9d11493038718b22f65447
Nicolas <Howe>
2017-10-12 15:36:10 -0400

    add svgs for social media logos

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const curTag = '#aboutMe';
+const display = '#aboutMeText';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
